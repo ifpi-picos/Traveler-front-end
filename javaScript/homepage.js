0082ab2b554d39b068ba99b0efdceee9b3366b83
AlessandroVianaSilva <silvaalessandro352@gmail.com>
2022-11-09 12:56:21 -0300

    v.0.1.0 - homepage

---
+++ javaScript/homepage.js
@@ -0,0 +1,43 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
+
