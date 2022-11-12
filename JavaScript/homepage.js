425bb6ae24f536e6d250be8e220c88824626f45f
wesley <wesleyvc2110@gmail.com>
2022-11-12 17:41:01 -0200

    feat: starting integration with api in home page

--- JavaScript/homepage.js
+++ JavaScript/homepage.js
@@ -1,3 +1,32 @@
+// const divAnnouncement = document.querySelector('#')
+
+// async function getAnnouncements() {
+//     const response = await fetch('https://traveler-yd39.onrender.com/announcement');
+//     const announcements = await response.json();
+//     fillScreen(announcements);
+// }
+
+// function fillScreen(announcements) {
+//     announcements.forEach(announcement => {
+//         const newAnnouncementHtml = `
+//             <div class="box" data-aos="fade-up">
+//                 <div class="image">
+//                     <img src="/img/Teresina.jpg" alt="">
+//                     <h3> <i class="fas fa-map-marker-alt"></i> teresina </h3>
+//                 </div>
+//                 <div class="content">
+//                     <div class="price"> R$${announcement.price} </div>
+//                     <p>picos para teresina</p>
+//                     <a href="#" class="btn"> acessar</a>
+//                 </div>
+//             </div>
+//         `
+//         divAnnouncement.innerHTML = divAnnouncement.innerHTML + newAnnouncementHtml;
+//     });
+// }
+
+// integraçao com a api (WESLEY)
+
 let navbar = document.querySelector('.navbar')
 
 document.querySelector('#menu-btn').onclick = () =>{
