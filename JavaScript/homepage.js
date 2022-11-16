addf0553cdfcdcaee93f16660ec432053072696f
edivan <wesleyvc2110@gmail.com>
2022-11-16 17:25:16 -0300

    trabalhando na integração da pagina homepage

--- JavaScript/homepage.js
+++ JavaScript/homepage.js
@@ -1,31 +1,38 @@
-// const divAnnouncement = document.querySelector('#')
+const divAnnouncement = document.querySelector('#announcement')
 
-// async function getAnnouncements() {
-//     const response = await fetch('https://traveler-yd39.onrender.com/announcement');
-//     const announcements = await response.json();
-//     fillScreen(announcements);
-// }
 
-// function fillScreen(announcements) {
-//     announcements.forEach(announcement => {
-//         const newAnnouncementHtml = `
-//             <div class="box" data-aos="fade-up">
-//                 <div class="image">
-//                     <img src="/img/Teresina.jpg" alt="">
-//                     <h3> <i class="fas fa-map-marker-alt"></i> teresina </h3>
-//                 </div>
-//                 <div class="content">
-//                     <div class="price"> R$${announcement.price} </div>
-//                     <p>picos para teresina</p>
-//                     <a href="#" class="btn"> acessar</a>
-//                 </div>
-//             </div>
-//         `
-//         divAnnouncement.innerHTML = divAnnouncement.innerHTML + newAnnouncementHtml;
-//     });
-// }
 
-// integraçao com a api (WESLEY)
+async function getAnnouncements() {
+    const response = await fetch('https://traveler-yd39.onrender.com/announcement');
+    const announcements = await response.json();
+    if (response.status === 200){
+        console.log(announcements);
+        fillScreen(announcements);
+    } else {
+        console.log(announcements.message);
+    }
+}
+
+function fillScreen(announcements) {
+    announcements.forEach(announcement => {
+        const newAnnouncementHtml = `
+            <div class="box" data-aos="fade-up">
+                <div class="image">
+                    <img src="/img/Teresina.jpg" alt="">
+                    <h3> <i class="fas fa-map-marker-alt"></i> teresina </h3>
+                </div>
+                <div class="content">
+                    <div class="price"> R$${announcement.price} </div>
+                    <p>picos para teresina</p>
+                    <a href="#" class="btn"> acessar</a>
+                </div>
+            </div>
+        `
+        divAnnouncement.innerHTML = divAnnouncement.innerHTML + newAnnouncementHtml;
+    });
+}
+
+getAnnouncements();
 
 let navbar = document.querySelector('.navbar')
 
