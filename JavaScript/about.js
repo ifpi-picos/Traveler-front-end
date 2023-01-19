92bf950be7a1a03c2fd9deeb56a991f0eba93c0f
AlessandroVianaSilva <silvaalessandro352@gmail.com>
2023-01-19 00:29:07 -0300

    pagina de sobre

---
+++ JavaScript/about.js
@@ -0,0 +1,66 @@
+const baseUrl = 'https://traveler-yd39.onrender.com/';
+// const baseUrl = 'http://localhost:3003/';
+
+if (localStorage.getItem('token') == null) {
+    alert("Você precisa estar logado para ter acesso a esta página.");
+    window.location.href = `https://traveler-io.netlify.app/`;
+} 
+
+
+
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () => {
+    themeBtn.classList.toggle('fa-sun');
+
+    if (themeBtn.classList.contains('fa-sun')) {
+        document.body.classList.add('active');
+    } else {
+        document.body.classList.remove('active');
+    }
+
+};
+
+
+var swiper = new Swiper(".about-slider", {
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
